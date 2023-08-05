a137a532bb210234c3b5f6b05ea36420c0c9d847
Chris Farris <chrisf@primeharbor.com>
2023-08-05 11:56:28 -0400

    Update es_snapshot.py

--- scripts/es_snapshot.py
+++ scripts/es_snapshot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2019-2020 Turner Broadcasting Inc. / WarnerMedia
+# Copyright 2023 Chris Farris <chris@primeharbor.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
