8e0e5b075b89e43c191ba14bebe598e7dae38068
Chris Farris <chrisf@primeharbor.com>
2023-08-05 11:56:19 -0400

    Update replay_sechub_findings.py

--- scripts/replay_sechub_findings.py
+++ scripts/replay_sechub_findings.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2023 Chris Farris <chrisf@primeharbor.com>
+# Copyright 2023 Chris Farris <chris@primeharbor.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
