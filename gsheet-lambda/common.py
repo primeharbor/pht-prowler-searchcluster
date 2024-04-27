13b5dd23c178ef0cca2aad063ddd99490a469c4c
Chris Farris <chrisf@primeharbor.com>
2024-04-27 16:00:41 -0400

    refactor common functions into shared file

--- gsheet-lambda/common.py
+++ gsheet-lambda/common.py
@@ -13,11 +13,16 @@
 # limitations under the License.
 
 from botocore.exceptions import ClientError
+from datetime import datetime
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from gspread.exceptions import WorksheetNotFound, APIError, SpreadsheetNotFound
+from time import sleep
+from urllib.parse import unquote
 import boto3
+import gspread
 import json
 import os
-from urllib.parse import unquote
-
 
 import logging
 logger = logging.getLogger()
@@ -25,6 +30,15 @@ logger.setLevel(getattr(logging, os.getenv('LOG_LEVEL', default='INFO')))
 logging.getLogger('botocore').setLevel(logging.WARNING)
 logging.getLogger('boto3').setLevel(logging.WARNING)
 logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+# Throttle Settings for Google Sheets
+BATCH_SIZE=250
+SLEEP_INTERVAL=10
+
+SCOPES = [
+    'https://www.googleapis.com/auth/drive',
+    'https://spreadsheets.google.com/feeds'
+]
 
 def get_object(bucket, obj_key):
     '''get the object to index from S3 and return the parsed json'''
@@ -41,3 +55,135 @@ def get_object(bucket, obj_key):
         else:
             logger.error("Error getting resource s3://{}/{}: {}".format(bucket, obj_key, e))
         return(None)
+
+def open_or_create_gsheet(sheet_name, worksheet_name, header_row):
+
+    google_secret = getSecret(os.environ['GSHEET_SECRET'])
+    new_folder_id = os.environ['GOOGLE_DRIVE_ID']
+
+    credentials = service_account.Credentials.from_service_account_info(google_secret, scopes=SCOPES)
+    gc = gspread.authorize(credentials)
+
+    logger.info(f"Attempting to open {sheet_name} using {google_secret['client_email']}")
+    try:
+        gsheet = gc.open(sheet_name)
+        return(gsheet)
+
+    except SpreadsheetNotFound:
+        logger.info(f"Google Sheet {sheet_name} doesn't exist, creating it.")
+
+        drive_service = build('drive', 'v3', credentials=credentials)
+        # new_folder_id = create_folder(drive_service, folder_name, parent_folder_id=os.environ['GOOGLE_DRIVE_ID'])
+        gsheet = gc.create(title=sheet_name, folder_id=new_folder_id)
+        # share_response = gsheet.share('chris@primeharbor.com', perm_type='user', role='writer', notify=True, email_message="You Have a New Scorecard")
+        # print(json.dumps(share_response.json(), default=str))
+        # permissionId = share_response.json()["id"]
+        # gsheet.transfer_ownership(permissionId)
+
+        worksheet = gsheet.add_worksheet(worksheet_name, 1, len(header_row), index=0)
+        worksheet.append_row(header_row, value_input_option='RAW', insert_data_option="INSERT_ROWS", include_values_in_response=False)
+
+        # Format the google sheet
+        sheetId = worksheet._properties['sheetId']
+        body = {
+            "requests": [
+                {
+                    "updateDimensionProperties": {
+                        "range": {
+                            "sheetId": sheetId,
+                            "dimension": "COLUMNS",
+                            "startIndex": 0,
+                            "endIndex": 2
+                        },
+                        "properties": {"hiddenByUser": True},
+                        "fields": "hiddenByUser"
+                    }
+                },
+                {
+                    "setBasicFilter":
+                    {
+                        "filter":
+                        {
+                        "range":
+                            {
+                                "sheetId": sheetId,
+                                "startColumnIndex": 0, #column A
+                                "endColumnIndex": len(header_row)
+                            }
+                        }
+                    }
+                },
+                {
+                    'updateSheetProperties': {
+                        'properties': {
+                            "sheetId": sheetId,
+                            'gridProperties': {'frozenRowCount': 1}},
+                        'fields': 'gridProperties.frozenRowCount',
+                    }
+                }
+            ]
+        }
+        res = gsheet.batch_update(body)
+
+        return(gsheet)
+
+    except Exception as e:
+        logger.critical(f"Failed to open Google Sheet: {e}")
+        return(None)
+
+def getSecret(secretName):
+    """
+    If a secret name is correctly passed in at runtime, in this class, this will using the sm reference,
+    go and retrieve the SecretString value and return it to the user
+    as a json body.
+    """
+    session = boto3.session.Session()
+    sm = session.client('secretsmanager')
+
+    try:
+        secretData = sm.get_secret_value(SecretId=secretName)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            logger.exception("Provided KMS Key Decryption Failure: {}".format(e))
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            logger.exception("Server Side Service Level Error: {}".format(e))
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            logger.exception("Invalid Value for Parameter: {}".format(e))
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            logger.exception("Invalid Request: {}".format(e))
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.exception("Requested Provided Does Not Exist: {}".format(e))
+            raise e
+        elif e.response['Error']['Code'] == 'AccessDeniedException':
+            logger.exception(f"Function doesn't have access to secret {secretName}: {e}")
+            raise e
+        else:
+            logger.exception(f"Undefined Error fetching secret: {secretName} Auth?")
+            raise e
+
+    if 'SecretString' in secretData:
+        secret = secretData['SecretString']  # dictionary
+    else:
+        import base64
+        secret = base64.b64decode(secretData['SecretBinary'])  # binary
+
+    secret = json.loads(secret)
+    return secret
+
+def write_to_gsheet(wks, rows):
+    try:
+        logger.debug(f"Writing {len(rows)} rows to google")
+        wks.append_rows(rows, value_input_option='RAW', insert_data_option="INSERT_ROWS", include_values_in_response=False)
+        return(len(rows))
+    except APIError as e:
+        if e.response.status_code == 429:
+            logger.warning(f"Getting Throttled. Sleeping for {SLEEP_INTERVAL}s {e.response.reason}")
+            sleep(SLEEP_INTERVAL)
+            return(write_to_gsheet(wks, rows))
+        else:
+            logger.error(f"Got API Error: {e.response.reason}")
+            raise
--- gsheet-lambda/prowler_scorecard.py
+++ gsheet-lambda/prowler_scorecard.py
@@ -36,14 +36,6 @@ logging.getLogger('urllib3').setLevel(logging.WARNING)
 
 HEADER_ROW = ['FindingUniqueId', 'AssessmentStartTime', 'AccountId', 'AccountName', 'CheckID', "Severity", 'Status', "CheckTitle", "ServiceName", "SubServiceName", "Region", "ResourceArn", "ResourceName",  "StatusExtended"]
 
-SCOPES = [
-    'https://www.googleapis.com/auth/drive',
-    'https://spreadsheets.google.com/feeds'
-]
-
-# Throttle Settings for Google Sheets
-BATCH_SIZE=250
-SLEEP_INTERVAL=10
 
 
 # Lambda execution starts here
@@ -88,12 +80,13 @@ def process_file(bucket, obj_key):
         return(None)
 
     sheet_name = f"Prowler-Scorecard-{today}"
-    gsheet = open_or_create_gsheet(sheet_name)
+    worksheet_name = f"All-Findings"
+    gsheet = open_or_create_gsheet(sheet_name, worksheet_name, HEADER_ROW)
+
     if gsheet is None:
         logger.critical(f"Failed to open {sheet_name}. Aborting...")
         raise Exception(f"Failed to open {sheet_name}. Aborting...")
 
-    worksheet_name = f"All-Findings"
     try:
         worksheet = gsheet.worksheet(worksheet_name)
     except WorksheetNotFound as e:
@@ -149,82 +142,6 @@ def process_prowler_ocsf(f):
     return(row)
 
 
-def open_or_create_gsheet(sheet_name):
-
-    google_secret = getSecret(os.environ['GSHEET_SECRET'])
-    new_folder_id = os.environ['GOOGLE_DRIVE_ID']
-    worksheet_name = f"All-Findings"
-
-    credentials = service_account.Credentials.from_service_account_info(google_secret, scopes=SCOPES)
-    gc = gspread.authorize(credentials)
-
-    logger.info(f"Attempting to open {sheet_name} using {google_secret['client_email']}")
-    try:
-        gsheet = gc.open(sheet_name)
-        return(gsheet)
-
-    except SpreadsheetNotFound:
-        logger.info(f"Google Sheet {sheet_name} doesn't exist, creating it.")
-
-        drive_service = build('drive', 'v3', credentials=credentials)
-        # new_folder_id = create_folder(drive_service, folder_name, parent_folder_id=os.environ['GOOGLE_DRIVE_ID'])
-        gsheet = gc.create(title=sheet_name, folder_id=new_folder_id)
-        # share_response = gsheet.share('chris@primeharbor.com', perm_type='user', role='writer', notify=True, email_message="You Have a New Scorecard")
-        # print(json.dumps(share_response.json(), default=str))
-        # permissionId = share_response.json()["id"]
-        # gsheet.transfer_ownership(permissionId)
-
-        worksheet = gsheet.add_worksheet(worksheet_name, 1, len(HEADER_ROW), index=0)
-        worksheet.append_row(HEADER_ROW, value_input_option='RAW', insert_data_option="INSERT_ROWS", include_values_in_response=False)
-
-        # Format the google sheet
-        sheetId = worksheet._properties['sheetId']
-        body = {
-            "requests": [
-                {
-                    "updateDimensionProperties": {
-                        "range": {
-                            "sheetId": sheetId,
-                            "dimension": "COLUMNS",
-                            "startIndex": 0,
-                            "endIndex": 2
-                        },
-                        "properties": {"hiddenByUser": True},
-                        "fields": "hiddenByUser"
-                    }
-                },
-                {
-                    "setBasicFilter":
-                    {
-                        "filter":
-                        {
-                        "range":
-                            {
-                                "sheetId": sheetId,
-                                "startColumnIndex": 0, #column A
-                                "endColumnIndex": len(HEADER_ROW)
-                            }
-                        }
-                    }
-                },
-                {
-                    'updateSheetProperties': {
-                        'properties': {
-                            "sheetId": sheetId,
-                            'gridProperties': {'frozenRowCount': 1}},
-                        'fields': 'gridProperties.frozenRowCount',
-                    }
-                }
-            ]
-        }
-        res = gsheet.batch_update(body)
-
-        return(gsheet)
-
-    except Exception as e:
-        logger.critical(f"Failed to open Google Sheet: {e}")
-        return(None)
-
 def process_prowler_native(f):
     if f['Status'] == "PASS" or f['Status'] == "INFO":
         return(None)
@@ -248,63 +165,4 @@ def process_prowler_native(f):
         f['StatusExtended'],
     ]
     return(row)
-
-
-def write_to_gsheet(wks, rows):
-    try:
-        logger.debug(f"Writing {len(rows)} rows to google")
-        wks.append_rows(rows, value_input_option='RAW', insert_data_option="INSERT_ROWS", include_values_in_response=False)
-        return(len(rows))
-    except APIError as e:
-        if e.response.status_code == 429:
-            logger.warning(f"Getting Throttled. Sleeping for {SLEEP_INTERVAL}s {e.response.reason}")
-            sleep(SLEEP_INTERVAL)
-            return(write_to_gsheet(wks, rows))
-        else:
-            logger.error(f"Got API Error: {e.response.reason}")
-            raise
-
-
-def getSecret(secretName):
-    """
-    If a secret name is correctly passed in at runtime, in this class, this will using the sm reference,
-    go and retrieve the SecretString value and return it to the user
-    as a json body.
-    """
-    session = boto3.session.Session()
-    sm = session.client('secretsmanager')
-
-    try:
-        secretData = sm.get_secret_value(SecretId=secretName)
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            logger.exception("Provided KMS Key Decryption Failure: {}".format(e))
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            logger.exception("Server Side Service Level Error: {}".format(e))
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            logger.exception("Invalid Value for Parameter: {}".format(e))
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            logger.exception("Invalid Request: {}".format(e))
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            logger.exception("Requested Provided Does Not Exist: {}".format(e))
-            raise e
-        elif e.response['Error']['Code'] == 'AccessDeniedException':
-            logger.exception(f"Function doesn't have access to secret {secretName}: {e}")
-            raise e
-        else:
-            logger.exception(f"Undefined Error fetching secret: {secretName} Auth?")
-            raise e
-
-    if 'SecretString' in secretData:
-        secret = secretData['SecretString']  # dictionary
-    else:
-        import base64
-        secret = base64.b64decode(secretData['SecretBinary'])  # binary
-
-    secret = json.loads(secret)
-    return secret
 
