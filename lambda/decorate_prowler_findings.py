c66a0dc85e6c4d32dfc0eaf6155c77203515f618
Andrew Kline <andrewckline@gmail.com>
2024-08-20 16:02:55 -0700

    add decorator lambda infrastructure (sqs, lambda role, alarms)

---
+++ lambda/decorate_prowler_findings.py
@@ -0,0 +1,34 @@
+# Copyright 2023 Chris Farris <chrisf@primeharbor.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import boto3
+import json
+import os
+
+from common import get_object
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(getattr(logging, os.getenv('LOG_LEVEL', default='INFO')))
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('boto3').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+
+# Lambda execution starts here
+def handler(event, context):
+    logger.debug("Received event: " + json.dumps(event, sort_keys=True))
+
+    return True
+
